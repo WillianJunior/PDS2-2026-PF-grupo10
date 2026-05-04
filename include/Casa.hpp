0a68b340ab8d96db91a1873dcfc392eedf1aa8b1
sofiasmithh <smithsofia628@gmail.com>
2026-05-04 15:51:05 -0300

    Create Casa.hpp

---
+++ include/Casa.hpp
@@ -0,0 +1,57 @@
+#ifndef CASA_HPP
+#define CASA_HPP
+
+#include "Dispositivo.hpp"
+
+/**
+ * @class Casa
+ * @brief Representa a residência e gerencia seus dispositivos.
+ *
+ * A classe Casa armazena os dispositivos cadastrados, permite adicionar,
+ * remover e buscar dispositivos pelo ID.
+ */
+class Casa {
+private:
+    static const int MAX_DISPOSITIVOS = 50;
+
+    Dispositivo* dispositivos[MAX_DISPOSITIVOS];
+    int qtdDispositivos;
+
+public:
+    /**
+     * @brief Construtor da classe Casa.
+     */
+    Casa();
+
+    /**
+     * @brief Destrutor da classe Casa.
+     */
+    ~Casa();
+
+    /**
+     * @brief Busca um dispositivo pelo seu ID.
+     * @param id ID do dispositivo procurado.
+     * @return Ponteiro para o dispositivo encontrado, ou nullptr se não existir.
+     */
+    Dispositivo* getDispositivo(int id);
+
+    /**
+     * @brief Adiciona um dispositivo à casa.
+     * @param dispositivo Ponteiro para o dispositivo que será adicionado.
+     */
+    void adicionarDispositivo(Dispositivo* dispositivo);
+
+    /**
+     * @brief Remove um dispositivo da casa pelo ID.
+     * @param id ID do dispositivo que será removido.
+     */
+    void removerDispositivo(int id);
+
+    /**
+     * @brief Retorna a quantidade atual de dispositivos cadastrados.
+     * @return Quantidade de dispositivos.
+     */
+    int getQtdDispositivos() const;
+};
+
+#endif
