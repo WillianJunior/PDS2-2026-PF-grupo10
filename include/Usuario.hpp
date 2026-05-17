e862fc9c75e3cf0dfdcc34edb3f5f7892108fb0d
luciano-ltda <legomesmoreira1510@hotmail.com>
2026-05-17 17:34:25 -0300

    Alterações no docs

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -13,28 +13,33 @@ private:
     std::string senha;  ///< Senha do usuário 
 
 public:
-    /** @brief Construtor de um usuario 
+    /** @brief Cria um usuário a partir do nome e senha escolhidos.
     * @param nome nome do usuario
     * @param senha senha do usuario */
     Usuario(std::string nome, std::string senha); 
 
-    /** @brief Destrutor da classe Usuario. */
+    /** @brief Remove o usuário cadastrado
+     *
+     */
     ~Usuario(); 
 
     /** @brief retorna o nome do usuário
-    * @return string nome armazenado no objeto */
-    std::string getNome(); 
+    * @return string nome cadastrado no sistema
+    */
+    std::string getNome() const;
 
-    /** @brief define o nome do usuário
-    * @param nome novo nome a ser atribuido */
+    /** @brief Altera o nome do usuário
+    * @param nome novo nome a ser atribuido
+    */
     void setNome(std::string nome);
 
     /** @brief Verifica se um usuário pode ser autenticado
     * Confere se existe um usuário com o nome e a senha informados
     * @param nome nome do usuário
     * @param senha senha do usuário
-    * @return true se as informações forem válidas */
-    bool autenticar(std::string nome, std::string senha); 
+    * @return true se as informações forem válidas
+    */
+    bool autenticar(std::string nome, std::string senha) const;
 };
 
 
