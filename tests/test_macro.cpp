218984263b5f6e31e288198d41f1f7648e596892
juliafietto <fiettojulia@gmail.com>
2026-05-18 14:07:48 -0300

    Test macro

--- tests/test_macro.cpp
+++ tests/test_macro.cpp
@@ -1,1 +1,38 @@
 #include "doctest.h"
+#include "../include/Macro.hpp"
+#include <type_traits>
+#include <string>
+
+TEST_CASE("Classe Macro existe") {
+    CHECK(sizeof(Macro) > 0);
+}
+
+TEST_CASE("Macro possui construtor com string") {
+    CHECK(std::is_constructible<Macro, std::string>::value);
+}
+
+TEST_CASE("Macro possui destrutor") {
+    CHECK(std::is_destructible<Macro>::value);
+}
+
+TEST_CASE("getEvento retorna string") {
+    CHECK(
+        std::is_same<
+            decltype(std::declval<Macro>().getEvento()),
+            std::string
+        >::value
+    );
+}
+
+TEST_CASE("getLista retorna Node*") {
+    CHECK(
+        std::is_same<
+            decltype(std::declval<Macro>().getLista()),
+            Node*
+        >::value
+    );
+}
+
+TEST_CASE("qtdMacros eh atributo estatico inteiro") {
+    CHECK(std::is_same<decltype(Macro::qtdMacros), int>::value);
+}
