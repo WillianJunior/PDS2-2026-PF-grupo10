f2ca88508787428f75d54687ddb2b18ff83dc9ae
juliafietto <fiettojulia@gmail.com>
2026-05-18 13:51:26 -0300

    test ar condicionado

--- tests/test_arcondicionado.cpp
+++ tests/test_arcondicionado.cpp
@@ -1,1 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
+
+#include "ArCondicionado.hpp"
+
+#include <type_traits>
+
+TEST_CASE("ArCondicionado herda de Dispositivo") {
+
+    CHECK(std::is_base_of<Dispositivo, ArCondicionado>::value);
+
+}
+
+TEST_CASE("Classe ArCondicionado existe") {
+
+    CHECK(sizeof(ArCondicionado) > 0);
+}
