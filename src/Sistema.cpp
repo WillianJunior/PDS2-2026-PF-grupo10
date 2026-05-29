b4bf803cd042343b9b9e4d6ca6ef5564ee69989c
juliafietto <fiettojulia@gmail.com>
2026-05-29 14:21:59 -0300

    Implementando .cpp

--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -1,35 +1,58 @@
 #include "Sistema.hpp"
+#include <iostream>
+#include <algorithm>
 
-Sistema::Sistema() {
+Sistema::Sistema() : ativo(false), sensor(new Sensor(0)) {
 }
 
 Sistema::~Sistema() {
+    for (Comodo* comodo : comodos) {
+        delete comodo;
+    }
+    comodos.clear();
+
+    if (sensor != nullptr) {
+        delete sensor;
+    }
 }
 
 Comodo* Sistema::getComodo(int i) const {
+    if (i >= 0 && i < static_cast<int>(comodos.size())) {
+        return comodos[i];
+    }
     return nullptr;
 }
 
-int Sistema::getQtdComodos(){
+int Sistema::getQtdComodos() {
     return comodos.size();
 }
 
 Sensor* Sistema::getSensor() const {
-    return nullptr;
+    return sensor;
 }
 
 bool Sistema::estaAtivo() const {
-    return false;
+    return ativo;
 }
 
 void Sistema::executarSistema() {
+    ativo = true;
 }
 
-void Sistema::receberComando(const string& comando) {
+void Sistema::receberComando(const std::string& comando) {
+    if (comando == "conectar" || comando == "ligar") {
+    }
 }
 
 void Sistema::adicionarComodo(Comodo& comodo) {
+    comodos.push_back(&comodo);
 }
 
 void Sistema::removerComodo(const Comodo& comodo) {
+    for (auto it = comodos.begin(); it != comodos.end(); ++it) {
+        if (*it == &comodo) {
+            comodos.erase(it);
+            break; 
+        }
+    }
 }
