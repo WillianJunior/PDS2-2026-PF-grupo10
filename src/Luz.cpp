ede9b2ddeb2f16456d6561030cc38db4bcc88c60
sofiasmithh <smithsofia628@gmail.com>
2026-05-29 13:44:42 -0300

    Implementacao da classe Luz

--- src/Luz.cpp
+++ src/Luz.cpp
@@ -1,24 +1,48 @@
 #include "Luz.hpp"
 #include <string>
 
+
 Luz::Luz(int id) : Dispositivo(id), intensidade(1) {
 }
 
+
 Luz::~Luz() {
 }
+
 
 int Luz::getIntensidade() const {
     return intensidade;
 }
 
-void Luz::ajustarIntensidade(int intensidade) {
-    // Implementação vazia
+
+void Luz::ajustarIntensidade(int novaIntensidade) {
+    if (novaIntensidade >= 1 && novaIntensidade <= 5) {
+        this->intensidade = novaIntensidade;
+    }
 }
+
 
 void Luz::detectarErro() {
-    // Implementação vazia
+    if (this->intensidade < 1 || this->intensidade > 5) {
+        this->erro = true;  
+    }
 }
 
+
 std::string Luz::getEstadoFormatado() const {
-    return "Luz: Desligada";  // Implementação vazia simples
+    std::string resposta;
+
+    
+    if (this->estado) {
+        resposta = "Luz [ID " + std::to_string(this->id) + "]: LIGADA - Intensidade: " + std::to_string(this->intensidade);
+    } else {
+        resposta = "Luz [ID " + std::to_string(this->id) + "]: DESLIGADA";
+    }
+
+    
+    if (this->erro) {
+        resposta += " (⚠️ ESTADO DE ERRO DETECTADO)";
+    }
+
+    return resposta;
 }
